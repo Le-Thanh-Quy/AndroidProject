305e5acd7f43862636e4a6c1a7a96a5e605a0a3d
huyhoang06 <hoangsndxqn>
2022-05-27 07:57:50 +0700

    [1] chat activity UI

---
+++ app/src/main/java/com/quy/chatapp/View/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.quy.chatapp.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.quy.chatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
