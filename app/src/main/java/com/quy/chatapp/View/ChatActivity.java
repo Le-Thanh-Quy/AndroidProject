aeecea4aff690946acc6741abe86c01c372918e6
huyhoang06 <hoangsndxqn@gmail.com>
2022-05-31 21:33:44 +0700

    [1] chat activity UI

--- app/src/main/java/com/quy/chatapp/View/ChatActivity.java
+++ app/src/main/java/com/quy/chatapp/View/ChatActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.quy.chatapp.R;
+import com.quy.chatapp.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity {
 
+    ActivityChatBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
