646fef05f4bc1c0a68ba6398ea65d09b82f335f6
huyhoang06 <hoangsndxqn@gmail.com>
2022-06-01 01:00:32 +0700

    [3] send text messages

---
+++ app/src/main/java/com/quy/chatapp/Model/Room.java
@@ -0,0 +1,81 @@
+package com.quy.chatapp.Model;
+
+public class Room {
+    private String roomID;
+    private String lastMess;
+    private String roomTimeLastMess;
+    private String imageRoom;
+    private String roomName;
+    private String roomType;
+    private boolean seen;
+
+    public Room() {
+    }
+
+    public Room(String roomID, String lastMess, String roomTimeLastMess, String imageRoom, String roomName, String roomType, String phoneNumber, boolean seen) {
+        this.roomID = roomID;
+        this.lastMess = lastMess;
+        this.roomTimeLastMess = roomTimeLastMess;
+        this.imageRoom = imageRoom;
+        this.roomName = roomName;
+        this.roomType = roomType;
+        this.seen = seen;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getRoomTimeLastMess() {
+        return roomTimeLastMess;
+    }
+
+    public void setRoomTimeLastMess(String roomTimeLastMess) {
+        this.roomTimeLastMess = roomTimeLastMess;
+    }
+
+    public String getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+
+    public String getLastMess() {
+        return lastMess;
+    }
+
+    public void setLastMess(String lastMess) {
+        this.lastMess = lastMess;
+    }
+
+    public String getImageRoom() {
+        return imageRoom;
+    }
+
+    public void setImageRoom(String imageRoom) {
+        this.imageRoom = imageRoom;
+    }
+
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+}
--- app/src/main/java/com/quy/chatapp/View/ChatActivity.java
+++ app/src/main/java/com/quy/chatapp/View/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.quy.chatapp.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -12,22 +13,19 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.quy.chatapp.R;
+import com.quy.chatapp.User;
 import com.quy.chatapp.databinding.ActivityChatBinding;
+import com.squareup.picasso.Picasso;
 
 public class ChatActivity extends AppCompatActivity {
-
-    ActivityChatBinding binding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityChatBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        uiEvent();
-
-    }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
@@ -43,6 +41,22 @@ public class ChatActivity extends AppCompatActivity {
         }
         return super.dispatchTouchEvent(ev);
 
+    }
+
+    ActivityChatBinding binding;
+    DatabaseReference reference;
+    User user;
+    String sendPhone = "+84384933378";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        uiEvent();
+        loadPage();
+        reference = FirebaseDatabase.getInstance().getReference();
+        user = User.getInstance();
     }
 
     private void uiEvent() {
@@ -83,4 +97,33 @@ public class ChatActivity extends AppCompatActivity {
         });
 
     }
+    private void loadPage() {
+        reference.child("Users").child(sendPhone).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                User receive_user = task.getResult().getValue(User.class);
+                assert receive_user != null;
+                binding.textName.setText(receive_user.getUserName());
+                Picasso.get().load(user.getUserAvatar()).into(binding.avatar);
+            }
+        });
+
+        reference.child("Users").child(sendPhone).child("status").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Boolean isOnline = snapshot.getValue(Boolean.class);
+                if(isOnline) {
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
